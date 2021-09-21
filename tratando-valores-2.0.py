5996ac3e6ae4a84269e99c597179a3e2298f4d81
rodrigo1708 <rodrigo_franca92@hotmail.com>
2021-09-20 22:03:01 -0300

    Atualização do código

---
+++ tratando-valores-2.0.py
@@ -0,0 +1,12 @@
+# Programa para ler vários números inteiros digitados pelo usuário. O programa deve parar quando o número 999 for
+# digitado e no final, o programa deve mostrar a soma dos números digitados e quantos números foram inseridos
+# excluindo o 999 da soma final e da contagem de números inseridos no programa.
+
+soma = n = c = 0
+while True:
+    n = float(input("Digite um valor [digite 999 para parar]: "))
+    c += 1
+    if n == 999:
+        break
+    soma += n
+print(f"Você digitou {c} números e a soma entre eles foi {soma}.")
