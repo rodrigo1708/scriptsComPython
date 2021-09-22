a91e2cc103a12958370aa4f44cb1d61e545d4df1
Rodrigo Santos de França <70596840+rodrigo1708@users.noreply.github.com>
2021-09-21 21:51:13 -0300

    Update par-ou-impar.py
    Adição de comentários ao código

--- par-ou-impar.py
+++ par-ou-impar.py
@@ -1,3 +1,5 @@
+# CRIAÇÃO DO PROGRAMA PAR OU ÍMPAR
+
 from random import randint
 from time import sleep
 vitoria = 0
@@ -5,13 +7,17 @@ print("="*30)
 print("VAMOS JOGAR PAR OU ÍMPAR!")
 print("="*30)
 sleep(1)
-while True:
+while True: # loop infinito caso o jogador vença o computador
+    # O jogador escolhe "par" ou "ímpar". Se digitar uma palavra, o código armazenará apenas a primeira letra, colocará em maiúscula e retira todos os espaços.
     escolha = str(input("Digite 'p' se quiser par ou 'i' se quiser ímpar: ")).strip().upper()[0]
+    # Caso a variável escolha seja preenchida errada, é criado o loop infinito até que o jogador digite uma entrada válida
     while escolha != 'P' and escolha != 'I':
         escolha = str(input("Digite uma opção válida! (P para PAR ou I para ÍMPAR): ")).strip().upper()[0]
     jogador = int(input("Qual número você quer jogar? "))
+    # O computador joga um número aleatório no intervalo de 0 a 10.
     pc = randint(0, 10)
     soma = jogador + pc
+    # Código para verificar se a soma dos valores jogados pelo jogador e pelo pc é par ou ímpar.
     if soma % 2 == 0:
         resultado = 'PAR'
     else:
@@ -20,6 +26,7 @@ while True:
     print(f'Você jogou {jogador} e o computador jogou {pc}. A soma deu {soma}. {resultado}!')
     print("-"*50)
     sleep(2)
+    # Se o resultado for igual à escolha do jogador (par ou ímpar), o jogador vence a primeira partida e o loop começa de novo.
     if resultado[0] == escolha:
         sleep(1)
         print("\o/"*5)
@@ -27,9 +34,11 @@ while True:
         print("=-="*15)
         sleep(2)
         vitoria += 1
+    # Caso o resultado seja diferente da escolha do jogador, o pc vence e o loop se encerra.
     else:
         break
 print("-_-"*15)
+# Em caso de vitória do pc, o programa conta quantaas vitórias o jogador teve até perder para o pc.
 if vitoria >= 5:
     print(f"GAME OVER!\nPARABÉNS você venceu{vitoria} vezes!!")
 else:
