7dba584a120033fc5f21454a61223c1917b9c94a
rodrigo1708 <rodrigo_franca92@hotmail.com>
2021-09-22 18:08:46 -0300

    Atualização do script (inserção de comentários)

--- compras.py
+++ compras.py
@@ -1,3 +1,5 @@
+# Programa para cadastro de compras e análise dos produtos cadastrados
+
 
 pmv = qtdprod = soma = pmil = 0
 # pmv = Produto Menor Valor, qtdprod = Quantidade de Produtos, pmil = Produtos acima de R$ 1.000,00
@@ -10,12 +12,16 @@ while True:
     while valor <= 0:
         valor = float(input("Digite um valor válido: R$ "))
     qtdprod += 1
+    # Soma da quantidade de produtos cadastrados
     soma += valor
+    # Soma dos valores cadastrados
     if valor > 1000:
         pmil += 1
+        # Contagem da quantidade de produtos que valem mais de mil reais
     if qtdprod == 1 or valor < pmv:
         pmv = valor
         npb = np
+        # Registro do valor do produto mais barato e nome do produto
     continuar = str(input("Deseja registrar mais produtos [S/N]? ")).strip().upper()[0]
     while continuar not in 'SN':
         continuar = str(input("Digite 'S' para registrar mais produtos"
